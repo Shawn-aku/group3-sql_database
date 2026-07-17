04b97c2640071045434ea919262785304520c6eb
Shawn-aku <s.bamigboye@alustudent.com>
2026-07-17 08:35:51 -0700

    student table

--- group3_db.sql
+++ group3_db.sql
@@ -1,1 +1,28 @@
 CREATE DATABASE ALU_GP3_DATABASE
+-- ================================
+-- Member A: Students Table
+-- ================================
+
+-- 1 & 2 & 3: CREATE TABLE with PK and FK
+CREATE TABLE Students (
+    student_id INT PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100),
+    classroom_id INT,
+    enrollment_date DATE,
+    FOREIGN KEY (classroom_id) REFERENCES Classroom(classroom_id)
+);
+
+-- 4: Insert at least 5 sample rows
+INSERT INTO Students VALUES (1, 'Shawn Dark', 'shawn@alu.edu', 1, '2024-09-01');
+INSERT INTO Students VALUES (2, 'Amara Noneik', 'amara@alu.edu', 1, '2024-09-01');
+INSERT INTO Students VALUES (3, 'Kofi Mesiah', 'kofi@alu.edu', 2, '2024-09-02');
+INSERT INTO Students VALUES (4, 'Fatima Nkosi', 'fatima@alu.edu', 2, '2024-09-03');
+INSERT INTO Students VALUES (5, 'Kyleeh shiga', 'kyleeh@alu.edu', 3, '2024-09-04');
+
+-- 5: UPDATE and DELETE
+UPDATE Students SET email = 'shawn.dark@alu.edu' WHERE student_id = 1;
+DELETE FROM Students WHERE student_id = 5;
+
+-- 6: SELECT with WHERE
+SELECT * FROM Students WHERE classroom_id = 1;
