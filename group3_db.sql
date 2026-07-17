f55cc00dfe8de07ed57add7a8f424094a09a88ec
Daniella <m.nishimwe@alustudent.com>
2026-07-17 16:38:34 +0200

    created the extracurricular table

--- group3_db.sql
+++ group3_db.sql
@@ -1,1 +1,7 @@
-CREATE DATABASE ALU_GP3_DATABASE
+CREATE DATABASE ALU_GRP3_DATABASE;
+	CREATE TABLE Extra_Curricular_Activities (
+		activity_id INT PRIMARY KEY AUTO-INCREMENT,
+		activity_name VARCHAR(100),
+		category VARCHAR(50),
+		FOREIGN KEY (faculty_advisor_id) REFERENCES Faculty(faculty_id)
+	);
